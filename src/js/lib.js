5ab68e8aad59463cded848b313ef42c99c001574
Vladislav Boretskii <9216370731@mail.ru>
2020-06-28 19:57:10 +0300

    changed bg-color

---
+++ src/js/lib.js
@@ -0,0 +1,17 @@
+
+
+
+// const logger = {
+//     keys() {
+//     console.log("Object Keys: ", Object.keys(this))
+// },
+
+
+
+// keysAndValues() {
+//     Object.keys(this).forEach(key =>{
+//         console.log(`"${key}": ${this[key]}`)
+//     })
+// }
+// }
+// logger.keysAndValues.call();
